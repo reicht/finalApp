2936daaf61a179616a065eb42471bd32db3ea719
Jordan Corley <rainwolfj@gmail.com>
2016-05-08 17:45:37 -0500

    Basic controller logic in place, now a simple views pass.

--- app/controllers/breeds_controller.rb
+++ app/controllers/breeds_controller.rb
@@ -1,22 +1,56 @@
 class BreedsController < ApplicationController
   def index
+    @breeds = Breed.all
   end
 
   def show
+    @breed = get_breed
   end
 
   def new
+    @breed = Breed.new
   end
 
   def create
+    @breed = Breed.new(breed_params)
+
+    if @breed.save
+      redirect_to @breed
+    else
+      render :new
+    end
   end
 
   def edit
+    @breed = get_breed
   end
 
   def update
+    @breed = get_breed
+
+    if @breed.update(breed_params)
+      redirect_to @breed
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @breed = get_breed
+
+    if @breed.destroy
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+  def get_breed
+    Breed.find(params.fetch(:id))
+  end
+
+  def breed_params
+    params.require(:breed).permit(:name, :size, :intro_year, :akc_rank, :energy_level, :group_id)
   end
 end
--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,22 +1,56 @@
 class DogsController < ApplicationController
   def index
+    @dogs = Dog.all
   end
 
   def show
+    @dog = get_dog
   end
 
   def new
+    @dog = Dog.new
   end
 
   def create
+    @dog = Dog.new(dog_params)
+
+    if @dog.save
+      redirect_to @dog
+    else
+      render :new
+    end
   end
 
   def edit
+    @dog = get_dog
   end
 
   def update
+    @dog = get_dog
+
+    if @dog.update(dog_params)
+      redirect_to @dog
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @dog = get_dog
+
+    if @dog.destroy
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+  def get_dog
+    Dog.find(params.fetch(:id))
+  end
+
+  def dog_params
+    params.require(:dog).permit(:name, :bio, :picture_url, :organization_id, :breed_id)
   end
 end
