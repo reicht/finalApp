779f86194317ac9bc20d1469ba85acec2368203b
Jordan Corley <rainwolfj@gmail.com>
2016-05-08 17:28:48 -0500

    Working on basic controller logics to get information to the views, organizations and groups in

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,7 +1,14 @@
 class GroupsController < ApplicationController
   def index
+    @groups = Group.all
   end
 
   def show
+    @group = get_group
+  end
+
+  private
+  def get_group
+    Group.find(params.fetch(:id))
   end
 end
--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,22 +1,57 @@
 class OrganizationsController < ApplicationController
+
   def index
+    @organizations = Organization.all
   end
 
   def show
+    @organization = get_organization
   end
 
   def new
+    @organization = Organization.new
   end
 
   def create
+    @organization = Organization.new(organization_params)
+
+    if @organization.save
+      redirect_to @organization
+    else
+      render :new
+    end
   end
 
   def edit
+    @organization = get_organization
   end
 
   def update
+    @organization = get_organization
+
+    if @organization.update(organization_params)
+      redirect_to @organization
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @organization = get_organization
+
+    if @organization.destroy
+      redirect_to root_path
+    else
+      redirect_to :back
+    end
+  end
+
+  private
+  def get_organization
+    Organization.find(params.fetch(:id))
+  end
+
+  def organization_params
+    params.require(:organization).permit(:name, :site_url, :description)
   end
 end
